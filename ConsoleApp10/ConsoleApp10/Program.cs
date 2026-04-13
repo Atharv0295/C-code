09b1edb3ee07a0f23e34253085cf831483fca7ee
Atharv Alapure <atharvalapure442@gmail.com>
2026-04-13 14:23:07 +0530

    pro010

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program10_SwapNumbers
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 10;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine(a + " " + b);
+    }
+}
