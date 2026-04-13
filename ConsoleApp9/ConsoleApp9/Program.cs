4606e90a4d7452d3d33b45549d6153c811df52a3
Atharv Alapure <atharvalapure442@gmail.com>
2026-04-13 14:20:55 +0530

    pro009

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program09_LargestOfThree
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 10;
+        int c = 7;
+
+        int max = Math.Max(a, Math.Max(b, c));
+
+        Console.WriteLine(max);
+    }
+}
