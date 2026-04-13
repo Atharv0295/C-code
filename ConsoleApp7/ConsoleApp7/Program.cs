fe6fca2fe1b03e16b5add0d6f545a47710726f9e
Atharv Alapure <atharvalapure442@gmail.com>
2026-04-13 14:15:58 +0530

    pro007

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program07_SimpleInterest
+{
+    static void Main()
+    {
+        double p = 1000;
+        double r = 5;
+        double t = 2;
+
+        double si = (p * r * t) / 100;
+
+        Console.WriteLine(si);
+    }
+}
