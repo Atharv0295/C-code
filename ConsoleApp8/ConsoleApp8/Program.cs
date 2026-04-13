e08b3be6fb19a885a32d3042fc18130ee9a0fc55
Atharv Alapure <atharvalapure442@gmail.com>
2026-04-13 14:18:31 +0530

    pro008

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program08_CompoundInterest
+{
+    static void Main()
+    {
+        double p = 1000;
+        double r = 5;
+        double t = 2;
+
+        double ci = p * Math.Pow((1 + r / 100), t) - p;
+
+        Console.WriteLine(ci);
+    }
+}
