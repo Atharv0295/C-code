f008a524da099b851ff2986e70228b5dcbd534b3
Atharv Alapure <atharvalapure442@gmail.com>
2026-04-20 13:51:21 +0530

    pro017

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 12345, count = 0;
+
+        while (num > 0)
+        {
+            num /= 10;
+            count++;
+        }
+
+        Console.WriteLine("Digits count = " + count);
+    }
+}
