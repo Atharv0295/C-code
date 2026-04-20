c553ca44f8e75bb961e02a102cf297dd94c48daf
Atharv Alapure <atharvalapure442@gmail.com>
2026-04-20 13:45:56 +0530

    pro014

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 121, temp, rev = 0;
+        temp = num;
+
+        while (num > 0)
+        {
+            int rem = num % 10;
+            rev = rev * 10 + rem;
+            num /= 10;
+        }
+
+        if (temp == rev)
+            Console.WriteLine("Palindrome");
+        else
+            Console.WriteLine("Not Palindrome");
+    }
+}
